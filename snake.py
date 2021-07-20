5221da72a18e99ea5f111b20a848f403cd8497d5
Mohsin <76669754+MohammadMohsinKhan@users.noreply.github.com>
2021-07-19 21:46:31 -0400

    Bug fixes for snake game!
    Fixed a ton of tiny bugs in snake game.
    some of the key improvements include the game window now scaling with your screen resolution and a new greatly improved system to detect collision between the snake head and game points.

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 # initialize libraries
+
 import pygame
 import os
 import sys
@@ -17,26 +18,28 @@ black = (0, 0, 0)
 yellow = (255,255,0)
 
 #set up a drawing window
-w = 400
-h = 400
+infoObject = pygame.display.Info()
+w = infoObject.current_w
+h = infoObject.current_h - 140
 screen = pygame.display.set_mode([w, h])
-screen.fill(white)
+screen.fill(black)
 
 # what state is the program in
-game_state = "game"
+game_state = "start"
 running = True
 
 px = random.randint(10, w-20)
 py = random.randint(10, h-20)
-pygame.draw.rect(screen, (red), (px,py, 10, 10 ))
+
 
 #variables used to generate the snake
-coord = [w//2, h//2,w//2+10, h//2,w//2+20, h//2,w//2+30, h//2,w//2+40, h//2,w//2+50, h//2, w//2+60, h//2, w//2+70, h//2, w//2+80, h//2]
+coord = [w//2, h//2,w//2+w//60, h//2,w//2+w//60, h//2,w//2+w//60, h//2,w//2+w//60, h//2,w//2+w//60, h//2, w//2+w//60, h//2, w//2+w//60, h//2, w//2+w//60, h//2]
 i = 1
 dx = 10
 x = 3
 direction = 'right'
 length = 20
+
 
 def display_text(string, colour, x, y):
     # create a font object.
@@ -52,6 +55,13 @@ def display_text(string, colour, x, y):
     textRect.center = (x, y)
 
     screen.blit(text, textRect)
+def start():
+    global game_state
+    screen.fill(black)
+    display_text("Press the space key to start!", red, w//2, h//2)
+    key = pygame.key.get_pressed()
+    if key[pygame.K_SPACE]:
+        game_state = 'game'
 
 def game():
     global coord, i, direction, dx, length, x
@@ -59,10 +69,9 @@ def game():
     global running
     global game_state
 
-    
     screen.fill(black)
 
-    pygame.draw.rect(screen, (green), (px,py, 10, 10 ))
+    pygame.draw.rect(screen, (green), (px,py, w//60, w//60 ))
 
     key = pygame.key.get_pressed()
     if key[pygame.K_ESCAPE]:
@@ -79,19 +88,19 @@ def game():
         length +=2
 
     if direction == 'right':
-        dx = 10
+        dx = w//60
         coord.append(coord[-2] + dx)
         coord.append(coord[-2])
     elif direction == 'left':
-        dx = -10
+        dx = -w//60 + 1
         coord.append(coord[-2] + dx)
         coord.append(coord[-2])
     elif direction == 'up':
-        dx = -10
+        dx = -w//60 + 1
         coord.append(coord[-2])
         coord.append(coord[-2] + dx)
     elif direction == 'down':
-        dx = 10
+        dx = w//60
         coord.append(coord[-2])
         coord.append(coord[-2] + dx)
     
@@ -106,9 +115,9 @@ def game():
             coord[-i] = h-5
 
         if i == 1:
-            pygame.draw.rect(screen, (red), (coord[-i-1],coord[-i], 10, 10 ))
+            pygame.draw.rect(screen, (red), (coord[-i-1],coord[-i], w//60, w//60 ))
         else:
-            pygame.draw.rect(screen, (blue), (coord[-i-1],coord[-i], 10, 10 ))
+            pygame.draw.rect(screen, (blue), (coord[-i-1],coord[-i], w//60, w//60 ))
         i = i + 2
     
     while x <= length:
@@ -116,17 +125,34 @@ def game():
             game_state = "end"
         x = x + 2
 
-    if px - 5 <= coord[-2] + 5 <= px + 15 and py - 5 <= coord[-1] + 5 <= py + 15:
-        length += 2
-        px = random.randint(100, w-50)
-        py = random.randint(50, h-50)
+    if direction == "right": 
+        if px <= (coord[-2] + (w//60))  <= (px + (w//60)) and py - 8  <= (coord[-1] + ((w//60)//2)) <= (py + 8 +(w//60)):
+            length += 2
+            px = random.randint(100, w-50)
+            py = random.randint(50, h-50)
+    elif direction == "left":
+        if px <= coord[-2]  <= (px + (w//60)) and py - 8  <= (coord[-1] + ((w//60)//2)) <= (py + 8 +(w//60)):
+            length += 2
+            px = random.randint(100, w-50)
+            py = random.randint(50, h-50)
+    elif direction == 'up':
+        if px - 8 <= (coord[-2] + (w//60)//2)  <= (px + 8 +(w//60)) and py  <= coord[-1] <= (py + (w//60)):
+            length += 2
+            px = random.randint(100, w-50)
+            py = random.randint(50, h-50)
+    else:
+        if px - 8 <= (coord[-2] + (w//60)//2)  <= (px + 8 +(w//60)) and py  <= (coord[-1] + (w//60)) <= (py + (w//60)):
+            length += 2
+            px = random.randint(100, w-50)
+            py = random.randint(50, h-50)
 
-    if len(coord) > length: #removes old values from the llist which are no longer needed
+    if len(coord) > length: #removes old values from the list which are no longer needed
         coord.pop(0)
         coord.pop(0)
     
     i = 1
     x = 3
+    
 
 def end():
     global game_state
@@ -134,25 +160,35 @@ def end():
     screen.fill(black)
     display_text("Press R to retry", white, w//2, h//2)
     display_text("score: "+str(length*10), red, w//2, h//2 + 50)
-
+    key = pygame.key.get_pressed()
     if key[pygame.K_r]:
         os.execl(sys.executable, sys.executable, *sys.argv)
+
+    
     
 #runs until the user presses the escape key to quit
-while running:
-    clock.tick(20)
-    
-    events = pygame.event.get()
-    if game_state == 'game':
-        game()
-    
-    elif game_state == "end":
-        end()
-    
-    key = pygame.key.get_pressed()
-    if key[pygame.K_ESCAPE]:
-        running = False
+def main():
+    global running
 
-    pygame.display.flip()
+    while running:
+        clock.tick(20)
+        
+        events = pygame.event.get()
+        if game_state == 'game':
+            game()
+
+        elif game_state == "end":
+            end()
+        
+        elif game_state == 'start':
+            start()
+        
+        key = pygame.key.get_pressed()
+        if key[pygame.K_ESCAPE]:
+            return None
+
+        pygame.display.flip()
+
+main()
 
 pygame.quit()
